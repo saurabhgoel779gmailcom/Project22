f5ad1180159acca3d9b3199a59237d909f13632d
Kaashvi Goel <82019344+saurabhgoel779gmailcom@users.noreply.github.com>
2021-07-24 19:32:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var grid = 50;
+var width = 1366;
+var carGroup1,logGroup1;
+var grassHeight = 100;
+var gameState = "play";
+var carAnimation, logAnimation, playerAnimation;
+var school;
+function preload()
+{
+
+}
+
+function setup() {
+  createCanvas(1366,700);
+  carGroup1 = new Group();
+  logGroup1 = new Group();
+  
+  for (var i=0;i<6;i++){
+    var bottomGrass1=createSprite(683,height-50-(i*400),width,grassHeight);
+    if(i%2==0){
+      var road=createSprite(683,height-150-(i*400)-grassHeight,width,300);
+      road.shapeColor="black";
+    }
+    bottomGrass1.shapeColor="green";
+  }
+ 
+   for(var i=0;i<40;i++){
+     cars=new Car(2);
+     carGroup1.add(cars.spt);
+   }
+   player=new Player(width/2,height-25);
+ }
+
+function draw() {
+  background("skyblue");
+  translate(0,-player.spt.y+height-150);
+  drawSprites();
+}
+
+function keyPressed(){
+  if(keyCode == UP_ARROW){
+    player.move(0,-2);
+  }
+  else if(keyCode == DOWN_ARROW){
+    player.move(0,2);
+  }
+  else if(keyCode == LEFT_ARROW){
+    player.move(-2,0);
+  }
+  else if(keyCode == RIGHT_ARROW){
+    player.move(2,0);
+  }
+}
